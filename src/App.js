9f31967c3825b37ee71206262df38394d4199b6b
unknown <surajsatav1994@gmail.com>
2023-05-28 01:09:42 +0530

    Header file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import './App.css';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className='App'>
-      <div className="header_logo">
-
-      </div>
-      <div className="header_search">
-
-      </div>
-      <div className="header_nav">
-        <div className="nav_item">
-          <span className='nav_itemLineOne'>Hello Guest</span>
-          <span className='nav_itemLineTwo'>SignIn</span>
-        </div>
-        <div className="nav_item">
-          <span className='nav_itemLineOne'>Your</span>
-          <span className='nav_itemLineTwo'>shop</span>
-        </div>
-        <div className="nav_item">
-          <ShoppingBasketIcon fontSize='large'/>
-          <span className='nav_itemLineTwo'>0</span>
-        </div>
-      </div>
+    <div className="App">
+     <Header />
     </div>
   );
 }
